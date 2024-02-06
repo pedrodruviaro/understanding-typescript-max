16b3dc4ecc38fdfca782159069a5fbeb57af8ef1
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-06 15:50:07 -0300

    add: section-01 && starter section-02

--- 2-ts-basics-and-types/course-starting-project/app.ts
+++ 2-ts-basics-and-types/course-starting-project/app.ts
@@ -1,1 +1,31 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number, showResult: boolean): number | undefined {
+  if (showResult) {
+    console.log(n1 + n2);
+  } else {
+    return n1 + n2;
+  }
+}
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+interface Person {
+  name: string;
+  age: number;
+  hobies: string[];
+  role: Role;
+}
+
+const person = {
+  name: "Peter",
+  age: 27,
+  hobies: ["code", "books"],
+  role: Role.ADMIN,
+};
+
+if (person.role === Role.ADMIN) {
+  // admin
+}
